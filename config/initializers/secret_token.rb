338edde86ad04ba2922b4b840b5a68951d4b4074
approaching236 <vgrato@gmail.com>
2013-12-11 15:16:34 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slacktivist::Application.config.secret_token = 'f45b33beac9b75aa38a03181837443f2ad1f2dceb189ed9d6e598c7f4dd33a48290857d1cc68d1f1f5626049a16d563eee7311b17beb079805adcbfd9a148c90'
